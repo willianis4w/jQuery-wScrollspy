2349073759c57efb767937eaa8aa5744a9fcdf7e
Willian Costa Souza <willian@voltsdigital.com.br>
2013-09-24 11:40:50 -0300

    compress

---
+++ jquery.scrollspy-min.js
@@ -0,0 +1,12 @@
+ /*!
+ * jQuery wScrollspy ( https://github.com/willianis4w/jQuery-wScrollspy )
+ * Copyright (c) 2013 Willian Costa Souza ( https://github.com/willianis4w )
+ * Version: 1.0
+ * Under MIT License
+ *
+ * Thanks to: Fernando Da2k ( https://github.com/fdaciuk ) for some pointers.
+ *
+ * Based on jQuery Boilerplate ( http://jqueryboilerplate.com )
+ *
+ */
+ (function(e,t,n,r){function o(t,n){this.element=t;this.settings=e.extend({},s,n);this._defaults=s;this._name=i;this.init()}var i="wScrollspy",s={};o.prototype={init:function(){this.lastId="";this.topMenu=e(this.element);this.topMenuHeight=this.topMenu.outerHeight();this.menuItems=this.topMenu.find("a");this.scrollItems=e(this.menuItems).map(function(){var t=e(e(this).attr("href"));if(t.length){return t}});this.initEvents()},initEvents:function(){var n=this;e(this.menuItems).on("click",function(t){t.preventDefault();var r=e(this).attr("href"),i=r==="#"?0:e(r).offset().top-n.topMenuHeight+1;e("html, body").stop().animate({scrollTop:i},700)});e(t).scroll(function(){var t=e(this).scrollTop()+n.topMenuHeight;var r=e(n.scrollItems).map(function(){if(e(this).offset().top<t){return this}});r=r[r.length-1];var i=r&&r.length?r[0].id:"";if(n.lastId!==i){n.lastId=i;e(n.menuItems).removeClass("active").filter("[href=#"+i+"]").addClass("active")}})}};e.fn[i]=function(t){return this.each(function(){if(!e.data(this,"plugin_"+i)){e.data(this,"plugin_"+i,new o(this,t))}})}})(jQuery,window,document);
---
+++ jquery.scrollspy.js
@@ -0,0 +1,115 @@
+ /*!
+ * jQuery wScrollspy ( https://github.com/willianis4w/jQuery-wScrollspy )
+ * Copyright (c) 2013 Willian Costa Souza ( https://github.com/willianis4w )
+ * Version: 1.0
+ * Under MIT License
+ *
+ * Thanks to: Fernando Da2k ( https://github.com/fdaciuk ) for some pointers.
+ *
+ * Usage:
+ *     // your links' wrapper
+ *     $( '#main-header' ).wScrollspy();
+ *
+ * Based on jQuery Boilerplate ( http://jqueryboilerplate.com )
+ *
+ */
+
+;(function ( $, window, document, undefined ) {
+
+		// Create the defaults once
+		var pluginName = "wScrollspy",
+			defaults = {};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+			this.element = element;
+
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
+
+		Plugin.prototype = {
+			init: function () {
+
+				this.lastId        = '';
+				this.topMenu       = $( this.element );
+				this.topMenuHeight = this.topMenu.outerHeight();
+
+				// All list items
+				this.menuItems     = this.topMenu.find( 'a' );
+
+				// Anchors corresponding to menu items
+				this.scrollItems   = $( this.menuItems ).map( function() {
+					var item = $( $( this ).attr( 'href' ) );
+					if( item.length ) {
+						return item;
+					}
+				});
+
+				// Events
+				this.initEvents();
+
+			},
+
+			initEvents: function() {
+
+				var self = this;
+
+				// Bind click handler to menu items
+				// so we can get a fancy scroll animation
+				$( this.menuItems ).on( 'click', function( event ) {
+
+					event.preventDefault();
+
+					var href = $( this ).attr( 'href' ),
+						offsetTop = href === '#' ? 0 : $( href ).offset().top - self.topMenuHeight + 1;
+
+					$( 'html, body' ).stop().animate({
+						scrollTop: offsetTop
+					}, 700);
+
+				});
+
+				// Bind to scroll
+				$( window ).scroll( function() {
+
+					// Get container scroll position
+					var fromTop = $( this ).scrollTop() + self.topMenuHeight;
+
+					// Get id of current scroll item
+					var cur = $( self.scrollItems ).map( function() {
+						if( $( this ).offset().top < fromTop ) {
+							return this;
+						}
+					});
+
+					// Get the id of the current element
+					cur = cur[cur.length-1];
+					var id = cur && cur.length ? cur[0].id : '';
+
+
+					if( self.lastId !== id ) {
+
+						self.lastId = id;
+
+						// Set/remove active class
+						$( self.menuItems )
+							.removeClass( 'active' )
+							.filter( '[href=#' + id + ']' ).addClass( 'active' );
+					}
+				});
+
+			}
+		};
+
+		$.fn[ pluginName ] = function ( options ) {
+			return this.each(function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+				}
+			});
+		};
+
+})( jQuery, window, document );
